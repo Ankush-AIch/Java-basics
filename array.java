680a2d365d48f8eaf2bd2c35d74bf7cc7e67c4f1
Ankush Aich <jayaaich48@gmail.com>
2025-07-06 16:45:27 +0530

    Update array.java
    This most basic program

--- array.java
+++ array.java
@@ -1,5 +1,6 @@
 public class array {
   public static void main(String[] args) {
+    System.out.println("This is 1d array");
    int [] a = new int[] {10,20,30};
     for (int i = 0; i < a.length; i++) {
       System.out.println(a[i]);
