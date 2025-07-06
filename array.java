d94d697f80b666af1a88903d9e0bbe5c99821cbf
Ankush Aich <jayaaich48@gmail.com>
2025-07-06 16:25:15 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,8 @@
+public class array {
+  public static void main(String[] args) {
+   int [] a = new int[] {10,20,30};
+    for (int i = 0; i < a.length; i++) {
+      System.out.println(a[i]);
+    }
+  }
+}
