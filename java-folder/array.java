9f5a693735aa244e619c626aef4bb3cf4ae43c6d
Ankush Aich <jayaaich48@gmail.com>
2025-07-07 18:55:58 +0530

    Add files via upload

---
+++ java-folder/array.java
@@ -0,0 +1,10 @@
+public class array {
+  public static void main(String[] args) {
+  int[] a = new int[10];
+  a[0] = 36;
+  a[5] = 67;
+  for (int i = 0; i < a.length; i++) {
+    System.out.println(a[i]);
+  }
+  }
+}
