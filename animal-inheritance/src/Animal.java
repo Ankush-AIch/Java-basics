a35791a20b51c83a94fb7142b4b008ca3c053ae1
Ankush Aich <jayaaich48@gmail.com>
2025-08-10 20:15:02 +0530

    Add files via upload

---
+++ animal-inheritance/src/Animal.java
@@ -0,0 +1,15 @@
+// parent class
+class Animal {
+    //function
+    void eat(){
+        System.out.println("this animal eats food");
+    }
+}
+// child class
+ class Dog extends Animal {
+    //function
+    void bark(){
+        System.out.println("dog barks");
+    }
+}
+
