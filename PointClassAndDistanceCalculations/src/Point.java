a21c399851b23ed8422f44a007d7881d272a894a
Ankush Aich <jayaaich48@gmail.com>
2025-08-18 21:21:14 +0530

    Add files via upload

---
+++ PointClassAndDistanceCalculations/src/Point.java
@@ -0,0 +1,49 @@
+public class Point {
+    //instance variables
+    private int x;
+    private int y;
+
+    //first empty constructor
+    public Point() {
+    }
+
+    ;
+
+    //second constructor to initialize the fields
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    //methods to print the distance
+    public double distance() {
+        return distance(0, 0);
+    }
+
+    public double distance(Point a) {
+        return distance(a.x, a.y);
+    }
+
+    public double distance(int x, int y) {
+
+        int xDiff = this.x - x;
+        int yDiff = this.y - y;
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+    }
+}
