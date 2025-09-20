6652ddd67c8927e94824e034573b04c06793c287
Ankush Aich <jayaaich48@gmail.com>
2025-09-20 18:21:04 +0530

    Add files via upload

--- animal-inheritance/src/Animal.java
+++ animal-inheritance/src/Animal.java
@@ -1,15 +1,37 @@
-// parent class
 class Animal {
-    //function
-    void eat(){
-        System.out.println("this animal eats food");
+    private String type;
+    private String size;
+    private double weight;
+
+    //default constructor.
+    Animal(){}
+
+    //adding constructor
+    public Animal(String type, String size, double weight) {
+        this.type = type;
+        this.size = size;
+        this.weight = weight;
     }
-}
-// child class
- class Dog extends Animal {
-    //function
-    void bark(){
-        System.out.println("dog barks");
+    //the toString method to print the data.
+
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "type='" + type + '\'' +
+                ", size='" + size + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    //method for move
+    public void move(String speed){
+        System.out.println(type + " moves " + speed);
+    }
+
+    //function of making noise.
+    public void makeNoise() {
+        System.out.println(type + " makes some kind of noise.");
     }
 }
 
--- animal-inheritance/src/Main.java
+++ animal-inheritance/src/Main.java
@@ -1,7 +1,17 @@
 public class Main{
     public static void main(String[] args) {
+        Animal animal = new Animal("Generic animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
+
         Dog dog = new Dog();
-        dog.eat();
-        dog.bark();
+        doAnimalStuff(dog, "fast");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _ ");
+
     }
 }
